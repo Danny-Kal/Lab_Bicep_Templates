3904f355e8dd279a81712741921dcd29d29dda18
Danny-Kal <92371122+Danny-Kal@users.noreply.github.com>
2025-02-27 19:03:21 -0700

    adding function code folder

--- Lab_Function_Code/AZ PowerShell Function with logging.ps1
+++ Lab_Function_Code/AZ PowerShell Function with logging.ps1
@@ -16,30 +16,20 @@ if (-not $subscriptionId -or -not $resourceGroup -or -not $templateUrl) {
 }
 
 try {
-    Write-Host "Starting Azure Function execution..."
-    Write-Host "Subscription ID: $subscriptionId"
-    Write-Host "Resource Group: $resourceGroup"
-    Write-Host "Template URL: $templateUrl"
-
     # import-module az.accounts
     # Authenticate with Azure (using Managed Identity or Service Principal)
-    Write-Host "Authenticating with Azure..."
     Connect-AzAccount -Identity
 
     # Set the context to the specified subscription
-    Write-Host "Setting Azure context to subscription ID: $subscriptionId"
     Set-AzContext -SubscriptionId $subscriptionId
 
     # Deploy the Bicep template
     $deploymentName = "framerDeployment-$(Get-Date -Format 'yyyyMMdd-HHmmss')"
-    Write-Host "Starting deployment with name: $deploymentName"
     $deployment = New-AzResourceGroupDeployment `
         -ResourceGroupName $resourceGroup `
         -Name $deploymentName `
         -TemplateUri $templateUrl `
         -Mode Incremental
-
-    Write-Host "Deployment started successfully. Deployment ID: $($deployment.DeploymentId)"
 
     # Return success response
     Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
@@ -51,9 +41,6 @@ try {
     })
 }
 catch {
-    Write-Host "An error occurred: $($_.Exception.Message)"
-    Write-Host "Stack Trace: $($_.Exception.StackTrace)"
-
     # Return error response
     Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
         StatusCode = [HttpStatusCode]::InternalServerError
